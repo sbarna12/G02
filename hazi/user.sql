6fbe27e2c9e6dd1155ac73104bd0682e536ed6ca
sbarna12 <152182859+sbarna12@users.noreply.github.com>
2024-04-23 20:56:05 +0200

    házifeladat

---
+++ hazi/user.sql
@@ -0,0 +1,2 @@
+CREATE USER mask_user WITHOUT login;
+Grant select on Vendeg_Mask to mask_user
---
+++ hazi/user_run.sql
@@ -0,0 +1,6 @@
+Execute as User='mask_user';
+SELECT * from Vendeg_Mask
+
+
+revert
+SELECT * FROM Vendeg
